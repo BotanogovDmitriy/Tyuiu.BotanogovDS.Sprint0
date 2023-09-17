fdcc075f1bf4451c76da0ea9a892548cfdbb2423
tyuiu.BotanogovDS@outlook.com <mr.botanogov@gmail.com>
2023-09-17 16:32:18 +0500

    Task1

---
+++ Tyuiu.BotanogovDS.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.BotanogovDS.Sprint0.Task1.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, world");
+            string name = "Дима";
+            int age = 17;
+            Console.WriteLine("Здравствуйте, меня ховут " + name + " мне " + age + "лет!");
+            Console.ReadKey();
+        }
+    }
+}
